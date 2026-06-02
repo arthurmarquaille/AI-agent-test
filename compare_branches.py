7e17b1da06ee264014c0ee0ccd822313def0decd
Arthur Marquaille <arthur.marquaille@upsideo.fr>
2026-06-02 15:32:23 +0200

    a

--- compare_branches.py
+++ compare_branches.py
@@ -1,11 +1,16 @@
 import subprocess
 
 def compare_git_branches(branch):
-    command = f"git diff  main {branch}"
+    command = f"git diff --unified=1 main {branch}"
     result = subprocess.run(command, shell=True, text=True, capture_output=True)
-    if result == '' :
+    
+    if result.stdout.strip() == '':
         print("No differences found between the branches.")
     else:
-        print(result.stdout)
+        # Affiche seulement les lignes modifiées avec les numéros de lignes
+        lines = result.stdout.split('\n')
+        for line in lines:
+            if line.startswith('@@') or line.startswith('+') or line.startswith('-'):
+                print(line)
         
 compare_git_branches('modif_print')
